d5a8e6b5cec096d84e82ed61e29584282046230c
gcourtney27 <gcourtney@student.neumont.edu>
2019-08-15 18:27:14 -0600

    Small changes to HomeController for DB input output testing

--- VideoGameCompendium/VideoGameCompendium/Controllers/HomeController.cs
+++ VideoGameCompendium/VideoGameCompendium/Controllers/HomeController.cs
@@ -15,7 +15,6 @@ namespace VideoGameCompendium.Controllers
 
         public IActionResult Index()
         {
-<<<<<<< HEAD
             //db.AddToCollection("UserIDTest1", "GameIDTest1");
             //db.AddToCollection("Bill", "Uncharted");
             //db.AddToCollection("Bill", "Battlefield");
@@ -29,10 +28,7 @@ namespace VideoGameCompendium.Controllers
 
             //db.AddToFavorites("UserIDTest1", "GameIDTest1");
             //db.RemoveFromFavorites("UserIDTest1", "GameIDTest1");
-=======
             Game g = db.GetGameByID(11659);
-
->>>>>>> 2335b40166f3b862cfc7d905cb89d9d370f98a9b
             return View();
         }
 
